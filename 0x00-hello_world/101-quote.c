312b45fc82505ff2d04d14c0c4d45df04dd27b14
Toniagentle <toniansikan@gmail.com>
2023-06-16 07:10:58 -0700

    printing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line standad erro
+ *
+ * Return: always (1)
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
