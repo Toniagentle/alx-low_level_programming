136c8d63c6c0bfa23e2bda066a416efebc6a6683
Toniagentle <toniansikan@gmail.com>
2023-06-16 05:28:46 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - to print the size of data types
+ *
+ * Return: always (0)
+ *
+ */
+init main(void)
+{
+	printf("size of a char: ");
+	printf("size of an int: ")
+	printf("size of long int: ")
+	printf("size of a long long int:")
+	printf("size of a float:")
+	return (0);
+}
