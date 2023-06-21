9a9cea85709aa134fcc138df3b55978ccc26a417
Toniagentle <toniansikan@gmail.com>
2023-06-21 16:44:54 -0700

    Computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is > than 0
+ * 0 and prints 0 if n is 0
+ * -1 and prints - if n is less than zero
+ */
+int prints_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}else if (n == 0)
+}
