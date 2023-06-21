04f3784592ed5f38daab15ae5d5b4d2bda6495c4
Toniagentle <toniansikan@gmail.com>
2023-06-21 08:13:32 -0700

    prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * main - prints _putchar
+ * Return: Always 0
  */
 
 int main(void)
@@ -12,7 +11,7 @@ _putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
-_putchar8('h');
+_putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
