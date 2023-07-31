e66d517749ba7a0b02caa1e36d193a3751095db8
Toniagentle <toniansikan@gmail.com>
2023-07-31 14:46:05 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,8 +2,8 @@
 
 /**
  * add_nodeint - adds a new node at the beginning of a linked list
- * @head: A pointer to the first node in the list
- * @n: data to put in the new node
+ * @head: a pointer to the first node in the list
+ * @n: data to insert in the new node
  *
  * Return: pointer to the new node, or NULL if it fails
  */
